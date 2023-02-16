b526f00ecf4a65d6f728cfe14b76797dabcd2296
timcardenuto <timcardenuto@gmail.com>
2023-02-16 10:39:46 -0500

    updates

--- geolocation/doa_geo_example1.py
+++ geolocation/doa_geo_example1.py
@@ -1,34 +1,35 @@
 # Passive Geolocation Homework 7 Problem 3 - Tim Cardenuto
+# TODO set plot axes to be based on min/max values that are being plotted. The matplotlib autosizing doesn't seem to work when I need it to be a box
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 plt.ion()
-plt.xlabel('west/east (nm)')
-plt.ylabel('south/north (nm)')
-plt.title('DOA Geolocation Estimation Using Iterated Least Squares')
-
+fig,axs = plt.subplots(1,1)
+axs.set_xlabel('west/east (nm)')
+axs.set_ylabel('south/north (nm)')
+axs.set_title('DOA Geolocation Estimation Using Iterated Least Squares')
 
 
 def calculateEllipse(xhat, P, alpha):
     """ Computes an Elliptical Error Probable (EEP), i.e. an error ellipse
         The center of the ellipse is at the measured or estimated location (xhat)
-        The shape (how elliptical or circular), orientation (angular rotation), and size (distance of semimajor/semiminor axis) is based on the estimated covariance matrix (P) and the desired containment (alpha). 
-        The covariance matrix (P) incorporates the errors of the original measurements used to estimate the center point. If there was zero error in the original measurements then the ellipse would be zero size (a point). 
-        The desired containment (alpha) is interpreted as meaning the ellipse should be scaled big enough to provide an alpha probability of the target residing within the ellipse. The higher you want this to be, the bigger the ellipse will be. Common examples of alpha are 0.50 or 0.95. 
+        The shape (how elliptical or circular), orientation (angular rotation), and size (distance of semimajor/semiminor axis) is based on the estimated covariance matrix (P) and the desired containment (alpha).
+        The covariance matrix (P) incorporates the errors of the original measurements used to estimate the center point. If there was zero error in the original measurements then the ellipse would be zero size (a point).
+        The desired containment (alpha) is interpreted as meaning the ellipse should be scaled big enough to provide an alpha probability of the target residing within the ellipse. The higher you want this to be, the bigger the ellipse will be. Common examples of alpha are 0.50 or 0.95.
     """
     # Calculate containment critical value k from alpha
-    k = -2 * np.log(1-alpha)    # for 95% EEP, k = 5.9915 
-    
+    k = -2 * np.log(1-alpha)    # for 95% EEP, k = 5.9915
+
     # Calculate ellipse shape/size
     eigenvalues,eigenvectors = np.linalg.eig(P)
     semimajor = np.sqrt(k*max(eigenvalues))
     semiminor = np.sqrt(k*min(eigenvalues))
 
-    # Create ellipse 
+    # Create ellipse
     theta = np.linspace(0,2*np.pi)
     ellipse = np.transpose(np.vstack([semimajor*np.cos(theta), semiminor*np.sin(theta)]))
-    
+
     # Rotate ellipse
     eigenvector = eigenvectors[:,1]
     angle = np.arctan2(eigenvector[1], eigenvector[0])
@@ -39,30 +40,30 @@ def calculateEllipse(xhat, P, alpha):
         [-np.sin(angle), np.cos(angle)]
         ])
     ellipse = ellipse @ R
-    
+
     # Shift ellipse to center point
     ellipse = np.hstack([
-        np.vstack(ellipse[:,0]+xhat[0]), 
+        np.vstack(ellipse[:,0]+xhat[0]),
         np.vstack(ellipse[:,1]+xhat[1])
         ])
-        
+
     print("Eigen Values = ",eigenvalues)
     print("Eigen Vectors = ",eigenvectors)
     print("Semi-major Axis = ",semimajor)
     print("Semi-minor Axis = ",semiminor)
     print("")
-    
+
     return ellipse
-    
-    
+
+
 # Sensor 1
 a1 = np.vstack([[0],[0]])                                   # Sensor location x,y (meters)
 theta1 = 50/180*np.pi                                       # DOA angle measurement (radians)
 sigma1 = 3*np.pi/180                                        # DOA angle error, i.e. standard deviation (radians)
 u1 = np.vstack([[np.cos(theta1)], [np.sin(theta1)]])        # unit vector along DOA
 M1 = np.identity(2) - u1@(u1.conj().transpose())            # Moore-Penrose matrix
-plt.plot(a1[0], a1[1], 'ok', label='Sensor 1')
-plt.plot([a1[0], 100*np.cos(theta1)+a1[0]],[a1[1], 100*np.sin(theta1)+a1[1]], 'k')
+axs.plot(a1[0], a1[1], 'ok', label='Sensor 1')
+axs.plot([a1[0], 100*np.cos(theta1)+a1[0]],[a1[1], 100*np.sin(theta1)+a1[1]], 'k')
 
 # Sensor 2
 a2 = np.vstack([[20],[0]])
@@ -70,8 +71,8 @@ theta2 = 70/180*np.pi
 sigma2 = 2*np.pi/180
 u2 = np.vstack([[np.cos(theta2)], [np.sin(theta2)]])
 M2 = np.identity(2) - u2@(u2.conj().transpose())
-plt.plot(a2[0], a2[1], 'ok', label='Sensor 2')
-plt.plot([a2[0], 100*np.cos(theta2)+a2[0]],[a2[1], 100*np.sin(theta2)+a2[1]], 'k')
+axs.plot(a2[0], a2[1], 'ok', label='Sensor 2')
+axs.plot([a2[0], 100*np.cos(theta2)+a2[0]],[a2[1], 100*np.sin(theta2)+a2[1]], 'k')
 
 # Sensor 3
 a3 = np.vstack([[60],[0]])
@@ -86,16 +87,15 @@ plt.plot([a3[0], 100*np.cos(theta3)+a3[0]],[a3[1], 100*np.sin(theta3)+a3[1]], 'k
 xhat = np.linalg.inv(M1+M2+M3)@(M1@a1+M2@a2+M3@a3)
 plt.plot(xhat[0], xhat[1], '*r', label='Initial Target Location Estimate w/ Moore-Penrose')
 
-
 # Final estimation by Iterated Least Squares algorithm
 theta =  np.vstack([        # measurements
-    [theta1], 
-    [theta2], 
+    [theta1],
+    [theta2],
     [theta3]
     ])
 R =  np.vstack([            # covariance matrix (measurement errors)
-    [sigma1*sigma1, 0, 0], 
-    [0, sigma2*sigma2, 0], 
+    [sigma1*sigma1, 0, 0],
+    [0, sigma2*sigma2, 0],
     [0, 0, sigma3*sigma3]
     ])
 
@@ -113,9 +113,9 @@ while (count < maxcount and max(error) > maxerror):
     #   H = (1/(((xhat-a)'*(xhat-a))^.5))*[-sin(thetahat) cos(thetahat)]
     #   P = inv(H'*inv(R)*H)
     #   xhat = xhat + P*H'*inv(R)*(theta-thetahat)
-    
+
     thetahat = np.vstack([
-        [np.arctan2((xhat[1]-a1[1]), (xhat[0]-a1[0]))], 
+        [np.arctan2((xhat[1]-a1[1]), (xhat[0]-a1[0]))],
         [np.arctan2((xhat[1]-a2[1]), (xhat[0]-a2[0]))],
         [np.arctan2((xhat[1]-a3[1]), (xhat[0]-a3[0]))]
         ])
@@ -131,11 +131,13 @@ while (count < maxcount and max(error) > maxerror):
     error = abs(xhatnew - xhat)
     xhat = xhatnew
     count = count+1
-    
+
     print("Error: ",error)
-    plt.plot(xhat[0], xhat[1], '*k', label="Estimate Update, Error={}".format(error))
-    plt.legend(bbox_to_anchor=(1, 0.5), loc='center left', shadow=True, fontsize='small')
-    plt.tight_layout()
+    axs.plot(xhat[0], xhat[1], '*k', label="Estimate Update, Error={}".format(error))
+    axs.legend(bbox_to_anchor=(1, 0.5), loc='center left', shadow=True, fontsize='small')
+    # plt.tight_layout()
+    axs.set(xlim=(-5, 100), ylim=(-5, 100))
+    axs.set_aspect('equal')
     plt.draw()
     #input("Press Enter to continue...")
     print("")
@@ -145,9 +147,9 @@ while (count < maxcount and max(error) > maxerror):
 ellipse = calculateEllipse(xhat, P, 0.95)
 
 # Plot estimated target location and EEP
-plt.plot(xhat[0], xhat[1], '*g', label='Final Target Location Estimate')
-plt.plot(ellipse[:,0],ellipse[:,1],'-g')
-plt.legend(bbox_to_anchor=(1, 0.5), loc='center left', shadow=True, fontsize='small')
+axs.plot(xhat[0], xhat[1], '*g', label='Final Target Location Estimate')
+axs.plot(ellipse[:,0],ellipse[:,1],'-g')
+axs.legend(bbox_to_anchor=(1, 0.5), loc='center left', shadow=True, fontsize='small')
+axs.set(xlim=(-5, 100), ylim=(-5, 100))
+axs.set_aspect('equal')
 input("Press Enter to continue...")
-
-
